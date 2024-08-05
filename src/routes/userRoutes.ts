e2a3d7fe99317a4e77ecd03f80b63e5149e94f1f
Matheus-Pereira-Silva <matheuspereiradasilva08@gmail.com>
2024-08-05 19:17:08 -0300

    refactor: Alteração da rota login para sign-in

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,19 +6,18 @@ import {
   deleteUser,
   loginUser,
   signUp,
-  getAllUsers // Certifique-se de que essa função está exportada em userController.ts
+  getAllUsers
 } from '../controllers/userController';
 import { authenticate } from '../middlewares/authMiddleware';
 import validate from '../middlewares/validateMiddleware';
-import { signUpSchema, loginSchema } from '../validations/userValadation'; // Certifique-se de que o loginSchema está definido
-
+import { signUpSchema, loginSchema } from '../validations/userValadation';
 const router = Router();
 
 router.post('/sign-up', validate(signUpSchema), signUp);
-router.post('/login', validate(loginSchema), loginUser); // Adicione validação se necessário
+router.post('/sign-in', validate(loginSchema), loginUser);
 router.get('/users', authenticate, getAllUsers);
-router.get('/users/:id', authenticate, getUser); // Para buscar um usuário por ID
-router.put('/users/:id', authenticate, validate(signUpSchema), updateUser); // Para atualizar um usuário
-router.delete('/users/:id', authenticate, deleteUser); // Para deletar um usuário
+router.get('/users/:id', authenticate, getUser);
+router.put('/users/:id', authenticate, validate(signUpSchema), updateUser);
+router.delete('/users/:id', authenticate, deleteUser);
 
 export default router;
